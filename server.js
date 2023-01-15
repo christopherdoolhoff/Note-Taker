0771c6e27c88609413f1541eb859480533d10101
CDoolhoff <109476450+christopherdoolhoff@users.noreply.github.com>
2023-01-15 12:55:04 -0500

    updated my files in routes and updated servier.js to access rountes.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const path = require('path');
-const fs = require ('fs');
+const apiRoutes = require('./routes/api');
+const htmlRoutes = require('./routes/html');
 
 const PORT = process.env.PORT || 3001;
 
@@ -9,14 +9,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
-
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
-
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+app.use(apiRoutes);
+app.use(htmlRoutes);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
