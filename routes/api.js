53f515a488f8c9f0bd1dcd15cbc562fddbdc7809
CDoolhoff <109476450+christopherdoolhoff@users.noreply.github.com>
2023-01-15 15:59:00 -0500

    added post route for notes.

--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,19 @@
 const ns = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
+// const fs = require('fs');
 const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 
 // GET Route for retrieving notes
-ns.get('/notes', (req, res) =>
+ns.get('/api/notes', (req, res) =>
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 );
 
-// POST Route for submitting feedback
-ns.post('/notes', (req, res) => {
-  // Destructuring assignment for the items in req.body
+// POST Route for submitting notes
+ns.post('/api/notes', (req, res) => {
   const { title, text} = req.body;
 
   // If all the required properties are present
-  if (title && test) {
+  if (title && text) {
     // Variable for the object we will save
     const newNote = {
       title,
@@ -33,5 +33,9 @@ ns.post('/notes', (req, res) => {
     res.json('Error in posting feedback');
   }
 });
+
+// ns.delete('/api/notes/:id', (req, res) =>
+//   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+// );
 
 module.exports = ns;
