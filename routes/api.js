3a7dd955cd85ae5c64aa2476afa076774a54b835
CDoolhoff <109476450+christopherdoolhoff@users.noreply.github.com>
2023-01-16 20:40:56 -0500

    updated get route and post route.

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const ns = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
-// const fs = require('fs');
+const fs = require('fs');
 const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 
 // GET Route for retrieving notes
@@ -12,13 +12,11 @@ ns.get('/api/notes', (req, res) =>
 ns.post('/api/notes', (req, res) => {
   const { title, text} = req.body;
 
-  // If all the required properties are present
   if (title && text) {
-    // Variable for the object we will save
     const newNote = {
       title,
       text,
-      note_id: uuidv4(),
+      id: uuidv4(),
     };
 
     readAndAppend(newNote, './db/db.json');
@@ -33,9 +31,5 @@ ns.post('/api/notes', (req, res) => {
     res.json('Error in posting feedback');
   }
 });
-
-// ns.delete('/api/notes/:id', (req, res) =>
-//   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
-// );
 
 module.exports = ns;
